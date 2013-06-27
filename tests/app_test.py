fff5916b33c23ada4bbdbcd13388be1ef1e09f32
Erik Price <erik.price16@gmail.com>
2013-06-26 21:05:38 -0400

    Add a few simple flask app tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import flask
+import json
+
 import translate
-import translate.app
+from translate.app import app
+from translate.app import views
 
-# TODO: Write this
+import requests
 
-def test_dummy():
-    assert True
+
+class TestApp():
+
+    def setup_class(self):
+        views.manager = translate.backend.BackendManager({})
+
+        app.config['TESTING'] = True
+
+        self.client = app.test_client()
+
+    def teardown_class(self):
+        pass
+
+    def test_render_index(self):
+        resp = self.client.get('/')
+
+        assert resp.status_code == 200
+
+    def test_render_api(self):
+        # redirects to /api/
+        assert self.client.get('/api').status_code == 301
+
+        assert self.client.get('/api/').status_code == 200
+
+    def test_404(self):
+        resp = self.client.get('/asdasdasdasd')
+
+        assert resp.status_code == 404
