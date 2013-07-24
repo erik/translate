9829bb14fdbdafc844993b11a6e201dda64efadd
Erik Price <erik.price16@gmail.com>
2013-07-24 12:52:30 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,3 +7,11 @@ set -e
 cd ../translate/docs
 make html
 cp build/html/* -R ../../translate-docs/
+
+# last commit hash
+COMMIT=`git rev-parse HEAD | cut -c -10`
+
+cd ../../translate-docs
+
+git add .
+git commit -am "Update: $COMMIT"
